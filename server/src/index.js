b289f1e842f9fc00db034172cf007a1e43646c92
Kaushick02 <kaushickmohanty55555@gmail.com>
2026-04-15 03:05:48 +0530

    vercel rewrite setup

--- server/src/index.js
+++ server/src/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+app.get('/', (req, res) => {
+  res.send("Flashmind API is running 🚀");
+});
+
 // Routes
 app.use('/api/decks', deckRoutes);
 app.use('/api/cards', cardRoutes);
